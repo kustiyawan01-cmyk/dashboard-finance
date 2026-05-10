9e1e0bb8cce2923033b77ca84360899c20a41a9f
kustiyawan01-cmyk <kustiyawan01@gmail.com>
2026-05-10 11:40:11 +0700

    fix next config dan rename middleware

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@
 const nextConfig = {
   // Mengizinkan IP lokalmu agar auto-refresh berfungsi kembali
   serverExternalPackages: [],
-  experimental: {
-    allowedDevOrigins: ['192.168.1.28', 'localhost'],
-  },
 };
 
 export default nextConfig;
